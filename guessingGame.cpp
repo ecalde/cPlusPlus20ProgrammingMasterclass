913bc8c2a064d5dbf1146cebe97293a689646ba1
Edwin J. Calderon <calderonedwin3@gmail.com>
2023-04-04 18:14:05 -0500

    Adding guessing game file

---
+++ guessingGame.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int guessingGame(){
+    int secretNum = 7;
+    int guess;
+    int guessCount = 0;
+    int guessLimit = 3;
+    bool outOfGuesses = false;
+
+    while(secretNum != guess && !outOfGuesses){
+        if(guessCount < guessLimit){
+            cout << "Enter guess: ";
+            cin >> guess;
+            guessCount++;
+        } else {
+            outOfGuesses = true;
+        }
+
+    }
+
+    if(outOfGuesses){
+        cout << "You Loose!" << endl;
+    } else {
+        cout << "You Win!" << endl;
+    }
+
+}
