5a879d2668d8f5824477845c9e1b6a78e6ce1e5c
Edwin J. Calderon <calderonedwin3@gmail.com>
2023-04-04 15:57:13 -0500

    Adding calculator file

---
+++ calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int calculator()
+{
+    double num1, num2;
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << num1 + num2;
+    return 0;
+}
