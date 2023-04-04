53bf781b0365a6fa04f50a7426ddb1da277f516e
Edwin J. Calderon <calderonedwin3@gmail.com>
2023-04-04 17:22:54 -0500

    Updating calculator file

--- calculator.cpp
+++ calculator.cpp
@@ -4,13 +4,29 @@ using namespace std;
 
 int calculator()
 {
-    double num1, num2;
+    int num1, num2;
+    char op;
+
     cout << "Enter first number: ";
     cin >> num1;
-
+    cout << "Enter operator: ";
+    cin >> op;
     cout << "Enter second number: ";
     cin >> num2;
+    int result = 0;
+    if(op == '+'){
+        result = num1 + num2;
+    } else if(op == '-'){
+        result = num1 - num2;
+    } else if(op == '/'){
+        result = num1 / num2;
+    } else if(op == '*'){
+        result = num1 * num2;
+    } else {
+        cout << "Invalid Operator\n";
+    }
 
-    cout << num1 + num2;
+    cout << result << endl;
+
     return 0;
 }
