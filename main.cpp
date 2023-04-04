80f4c826325fd7d76078caa256b9140396535acd
Edwin J. Calderon <calderonedwin3@gmail.com>
2023-04-03 22:00:54 -0500

    Adding main.cpp variables.cpp and CMakeList.txt files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "variables.cpp"
+using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    // variables();
     return 0;
 }
+
+
---
+++ variables.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Edwin Calderon on 4/3/23.
+//
+using namespace std;
+
+int variables() {
+    string characterName = "Edwin";
+    int characterAge;
+    characterAge = 35;
+    cout << "There once was a man named " << characterName << endl;
+    cout << "He was " << characterAge << " years old " << endl;
+    cout << "He liked the name " << characterName << endl;
+    cout << "But did not like being " << characterAge << endl;
+    return 0;
+}
