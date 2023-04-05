0a3d83fb1675821f5bcd6d97e01868c1ba8fa543
Edwin J. Calderon <calderonedwin3@gmail.com>
2023-04-04 22:56:47 -0500

    Updating main file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,52 @@
 #include <iostream>
 #include "variables.cpp"
+#include "dataTypes.cpp"
+#include "workingWithNumbers.cpp"
+#include "gettingUserInput.cpp"
+#include "calculator.cpp"
+#include "arrays.cpp"
+#include "functions.cpp"
+#include "returns.cpp"
+#include "ifStatements.cpp"
+#include "switches.cpp"
+#include "whileLoops.cpp"
+#include "guessingGame.cpp"
+#include "forLoops.cpp"
+#include "twoDimensionalArraysAndNestedLoops.cpp"
+#include "pointers.cpp"
+#include "classesAndObjects.cpp"
+#include "constructorFunctions.cpp"
+#include "objectFunctions.cpp"
+#include "gettersAndSetters.cpp"
+#include "inheritance.cpp"
+
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
     // variables();
+    // strings();
+    // workingWithNumbers();
+    // gettingUserInput();
+    // gettingUserName();
+    // calculator();
+    // arrays();
+    // functions("Edwin", 29);
+    // returns(5.0);
+    // ifStatements();
+    // getMax(200, 50,10);
+    // switches(3);
+    // whileLoops();
+    // guessingGame();
+    // forLoops();
+    // twoDimensionalArraysAndNestedLoops();
+    // pointers();
+    // classesAndObjects();
+    // constructorFunctions();
+    // objectFunctions();
+    // gettersAndSetters();
+    // inheritance();
+
     return 0;
 }
 
